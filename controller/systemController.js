29ab1079871745f3797ff8f8f456ea9c71bd5b2e
cmkwong <cheung.mankwong@yahoo.com.hk>
2021-10-28 00:16:56 +0800

    6: small bug find -> findOne on finding version

--- controller/systemController.js
+++ controller/systemController.js
@@ -2,7 +2,7 @@ const catchAsync = require("../utils/catchAsync");
 const Version = require("../models/versionModel");
 
 exports.getVersion = catchAsync(async (req, res, next) => {
-  let version = await Version.find({});
+  let version = await Version.findOne({});
   res.status(200).json({
     data: version,
   });
