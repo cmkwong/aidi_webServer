4cce803bee4fac39f1a5a73f6f8869cd763a7a1a
cmkwong <cheung.mankwong@yahoo.com.hk>
2021-10-28 00:14:46 +0800

    6: small fixed

--- controller/systemController.js
+++ controller/systemController.js
@@ -3,7 +3,6 @@ const Version = require("../models/versionModel");
 
 exports.getVersion = catchAsync(async (req, res, next) => {
   let version = await Version.find({});
-  console.log(version);
   res.status(200).json({
     data: version,
   });
