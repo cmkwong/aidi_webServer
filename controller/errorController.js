90493d0b19679a891aaa3c4bb9af89ccfdefc72b
cmkwong <cheung.mankwong@yahoo.com.hk>
2021-10-28 09:51:17 +0800

    6 - print err message

--- controller/errorController.js
+++ controller/errorController.js
@@ -5,6 +5,6 @@ module.exports = (err, req, res, next) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
-    // stackTrace: err.stack,
+    stackTrace: err.stack,
   });
 };
