3a9006a3fb80ef8fa55036b97168337301afaa4e
cmkwong <cheung.mankwong@yahoo.com.hk>
2021-11-15 07:30:49 +0800

    able the getPrjStatus() fn

--- public/js/prjStatus.js
+++ public/js/prjStatus.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-// export const getPrjStatus = async (project_id, locale) => {
-//   try {
-//     const res = await axios({
-//       method: "GET",
-//       url: `/api/v1/project/status?project_id=${project_id}&locale=${locale}`,
-//     });
-//     if (res.status === 200) {
-//       return res.data.prjStatus_data;
-//     }
-//   } catch (err) {
-//     document.querySelector(".message").innerText = err.response.data.message;
-//   }
-// };
+export const getPrjStatus = async (project_id, locale) => {
+  try {
+    const res = await axios({
+      method: "GET",
+      url: `/api/v1/project/status?project_id=${project_id}&locale=${locale}`,
+    });
+    if (res.status === 200) {
+      return res.data.prjStatus_data;
+    }
+  } catch (err) {
+    document.querySelector(".message").innerText = err.response.data.message;
+  }
+};
 
 export const initPrjStatus = () => {
   // init the status
